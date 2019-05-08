bd8a73dd9dcd7e885c08333be54c97ccf1165259
青烟 <catish@outlook.com>
2019-05-08 08:00:40 +0800

    Create login_notice_f or_bash.sh

---
+++ login_notice_f or_bash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash                                      
+#================================================
+==                                               
+# OS Passed:    CentOS7                          
+# Description:  bash登录时自动发送登录提醒       
+# Author:       kuretru < kuretru@gmail.com >    
+# Github:       https://github.com/kuretru/Script
+-Collection                                      
+# Version:      1.1.170725                       
+#================================================
+==                                               
+                                                 
+#Server酱调用密钥                                
+key='xxxxxxxxxxxxxxx'                                      
+                                                 
+user=$(whoami)                    
+hostname=$(hostname)                      
+ip=$(strings /var/log/lastlog | grep -vE "192.168
+"\|"\pts/*")                             
+now=$(date "+%Y.%m.%d_%H.%M.%S")                 
+if [[ $ip =~ ^([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$ ]]                                             
+        then wget -q --spider https://sc.ftqq.com
+/${key}.send?text="${hostname}登录提醒"\&desp="IP
+地址：${ip}                                      
+                                                 
+用户名：${user}                                  
+                                                 
+登陆时间：${now}"                                
+else                                           
+        echo "“局域网登陆”"                      
+fi
